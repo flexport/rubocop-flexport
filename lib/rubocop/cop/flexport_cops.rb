49d76f21653e414b41038c783b6122bf5c24014f
Buildkite <dev@flexport.com>
2019-12-03 16:55:13 -0600

    Add EngineApiBoundary cop

--- lib/rubocop/cop/flexport_cops.rb
+++ lib/rubocop/cop/flexport_cops.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'mixin/engine_api'
+
+require_relative 'flexport/engine_api_boundary'
 require_relative 'flexport/global_model_access_from_engine'
 require_relative 'flexport/new_global_model'
