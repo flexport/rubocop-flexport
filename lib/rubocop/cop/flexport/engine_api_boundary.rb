88a00a353394ea188af592276f2706b875f78a09
Helen Stockman <helen@flexport.com>
2020-06-24 12:32:33 -0400

    Add allowlist to engine boundary rubocop

--- lib/rubocop/cop/flexport/engine_api_boundary.rb
+++ lib/rubocop/cop/flexport/engine_api_boundary.rb
@@ -17,7 +17,7 @@ module RuboCop
       #   will be accessible outside your engine. For example, adding
       #   `api/foo_service.rb` will allow code outside your engine to
       #   invoke eg `MyEngine::Api::FooService.bar(baz)`.
-      # - Create a `_whitelist.rb` file in `api/`. Modules listed in
+      # - Create an `allowlist.rb` or `_whitelist.rb` file in `api/`. Modules listed in
       #   this file are accessible to code outside the engine. The file
       #   must have this name and a particular format (see below).
       #
@@ -268,7 +268,8 @@ module RuboCop
           (
             in_legacy_dependent_file?(accessed_engine) ||
             through_api?(node) ||
-            whitelisted?(node, accessed_engine)
+            whitelisted?(node, accessed_engine) ||
+            allowlisted?(node, accessed_engine)
           )
         end
 
@@ -324,6 +325,23 @@ module RuboCop
 
         def through_api?(node)
           node.parent&.const_type? && node.parent.children.last == :Api
+        end
+
+        def allowlisted?(node, engine)
+          allowlist = read_api_file(engine, :allowlist)
+          return false if allowlist.empty?
+
+          depth = 0
+          max_depth = 5
+          while node.const_type? && depth < max_depth
+            full_const_name = remove_leading_colons(node.source)
+            return true if allowlist.include?(full_const_name)
+
+            node = node.parent
+            depth += 1
+          end
+
+          false
         end
 
         def whitelisted?(node, engine)
